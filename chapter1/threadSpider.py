4b5fb2d58b0c25e416a9477160511632a992e39d
cmustard06 <cmustard06@gmail.com>
2017-03-30 11:55:27 +0800

    可读性增强

--- chapter1/threadSpider.py
+++ chapter1/threadSpider.py
@@ -2,102 +2,128 @@
 
 __author__ = 'cmustard'
 
-"""
-多线程爬虫爬取python官方文档
-"""
-from Queue import Queue
-from threading import Thread, Lock
-import urlparse
+
 import socket
-import re
 import time
+import re
+import urlparse
+import Queue
+from threading import Thread, Lock
+
 
 seen_urls = set(['/'])
 lock = Lock()
 
-
 class Fetcher(Thread):
-
+    """
+    页面抓取
+    """
     def __init__(self, tasks):
-        # Thread.__init__(self)
         super(Fetcher, self).__init__()
+        self.daemon = True  # 开启守护线程
         self.tasks = tasks
-        self.daemon = True
 
         self.start()
 
     def run(self):
         while True:
             url = self.tasks.get()
-            print("[==>] {}".format("http://localhost.com:3000"+url))
             sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             sock.connect(('localhost.com', 3000))
-            get = 'GET {} HTTP/1.0\r\nHost:localhost.com:3000\r\n\r\n'.format(url)
+            get = '''GET {} HTTP/1.0\r\nHost: localhost.com:3000\r\n\r\n'''.format(url)
             sock.send(get)
             response = ''
             chunk = sock.recv(4096)
             while chunk:
-                response += chunk
+                response +=chunk
                 chunk = sock.recv(4096)
+            # 分析链接地址
+            links = self.parse_link(url, response)
 
-            links = self.parse_links(url, response)
-
+            # 判断链接是否已经被爬取
             lock.acquire()
-            for link in links.difference(seen_urls): # 属于links但是不属于seen_urls,即新的url
+            for link in links.difference(seen_urls):
                 self.tasks.put(link)
-            seen_urls.update(links)  # 列表就用update
+            seen_urls.update(links)
             lock.release()
             self.tasks.task_done()
 
-    def parse_links(self, fetched_url, response):
+    def parse_link(self, fetched_link, response):
+        """
+        :param fetched_link:
+        :param response:
+        :return: 分析完成后的链接集合
+        """
         if not response:
-            print('error:{}'.format(fetched_url))
+            print("[!!!]error url: {}".format(fetched_link))
             return set()
-        if not self._is_html(response):
-            return set()
-        urls = re.findall(r'''href=["']?([^\s"'<>]+)''', self.body(response))
 
+        is_html, code = self._is_html(response)
+        print("[==>] {} {} {}".format(time.asctime(), code, 'http://localhost.com:3000'+fetched_link))
+        if not is_html:
+            return set()
+
+        urls = re.findall(r'''href=["']?([^\s"'<>]+)''', self.get_body(response))
         links = set()
-        for url in urls:
-            normalized = urlparse.urljoin(fetched_url, url)
-            parts = urlparse.urlparse(normalized)
-            if parts.scheme not in ('', 'http', 'https'):
-                continue
-            host, port = parts.hostname, parts.port
-            if host and host.lower() not in ('localhost'):
-                continue
-            # 除去frag，即#号后面的字符串
-            defragmented, frag = urlparse.urldefrag(parts.path)
-            links.add(defragmented)
 
+        # 开始分析获取到的url是否是所需要的
+        for url in urls:
+            url = urlparse.urljoin(fetched_link, url)
+            urlp = urlparse.urlparse(url)
+            if urlp.scheme not in ('', 'http', 'https'):
+                continue
+            host, port = urlp.hostname, urlp.port
+            if host and host.lower() not in ('localhost.com'):
+                continue
+            # 除去#后的frag
+            # print url
+            defrag, frag = urlparse.urldefrag(url)
+            links.add(defrag)
         return links
 
-    def body(self, response):
-        body = response.split('\r\n\r\n')[1]
-        return body
-
     def _is_html(self, response):
-        head, body = response.split('\r\n\r\n')
-        headers = dict([head.split(': ') for head in head.split('\r\n')][1:])
+        """
+        判断抓取到的内容是否是text/html
+        :param response:
+        :return: boolean,code
+        """
+        header, body = response.split('\r\n\r\n')
+        headers = dict([head.split(': ') for head in header.split('\r\n')][1:])
+        code = header.split('\r\n')[0]
+        return ('text/html' in headers.get('Content-Type'), code)
 
-        return 'text/html' in headers.get('Content-Type')
+    def get_body(self, response):
+        """
+        获取服务器返回内容中的html部分
+        :param response:
+        :return: body
+        """
+        body = response.split("\r\n\r\n")[1]
+        return body
 
 
 class ThreadPool(object):
     def __init__(self, num_threads):
-        self.tasks = Queue()
-        for _ in range(num_threads):
+        self.num_threads = num_threads
+        self.tasks = Queue.Queue()
+        for _ in range(self.num_threads):
             Fetcher(self.tasks)
 
-    def add_task(self, url):
-        self.tasks.put(url)
+    def add_task(self, task):
+        self.tasks.put(task)
 
     def wait_completion(self):
-        self.tasks.join()
+        self.tasks.join()  # 阻塞直到队列中所有项目都已经被得到和处理
+
+
+def run():
+    start = time.time()
+    th = ThreadPool(4)
+    th.add_task('/')
+    th.wait_completion()
+    print("[***] Finish !!! speed time {:.2f}s, total collect {} urls".format(time.time()-start, len(seen_urls)))
+
 
 if __name__ == '__main__':
-    start = time.time()
-    pool = ThreadPool(4)
-    pool.add_task("/")
-    pool.wait_completion()
-    print('{} URLs fetched in {:.1f} seconds'.format(len(seen_urls), time.time()-start))
+    print("[***] Start crawling  pages!!!")
+    run()
