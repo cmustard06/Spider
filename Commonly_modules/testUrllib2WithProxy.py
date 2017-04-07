c3c20f0e7f8665019f5494ab2a3202da9daf73aa
cmustard06 <cmustard06@gmail.com>
2017-04-07 16:50:11 +0800

    重写代码

--- Commonly_modules/testUrllib2WithProxy.py
+++ Commonly_modules/testUrllib2WithProxy.py
@@ -1,112 +1,108 @@
+#!/usr/bin/env python
 # coding:utf-8
 
 __author__ = 'cmustard'
-
-"""
-使用代理模式访问网站
-"""
-
-import sys
+'''
+用于测试代理是否可用
+'''
 import urllib2
 import re
-
-
-def testArgument():
-    """
-    测试输入参数，只需要一个参数
-    :return:
-    """
-    if len(sys.argv) != 2:
-        print(u'只需要一个参数就够了')
-        tipUse()
-        exit()
-    else:
-        TP = TestProxy(sys.argv[1])
-
+import sys
+import urlparse
 
 def tipUse():
     """
-    显示提示信息
+    帮助文档
     :return:
     """
     print(u'该程序只能输入一个参数，这个参数必须是一个可用的proxy')
-    print(u'usage: python testUrllib2WithProxy.py http://1.2.3.4"5')
-    print(u'usage: python testUrllib2WithProxy.py https://1.2.3.4:5')
+    print(u'python testUrllib2WithProxy.py http://1.2.3.4:8080')
+    print(u'python testUrllib2WithProxy.py https://1.2.3.4:9999')
 
+def testArgs():
+    """
+    参数验证
+    :return:
+    """
+    if len(sys.argv)!=2:
+        print(u'只需要一个参数就够了！！！')
+        tipUse()
+        return False
+    else:
+        return True
 
 class TestProxy(object):
+    """
+    主程序，测试代理是否可用
+    """
     def __init__(self, proxy):
         self.proxy = proxy
-        self.checkProxyFormat(self.proxy)
         self.url = 'http://www.google.com'
-        self.timeout = 5
         self.flagWord = u'google'
-        self.useProxy(self.proxy)
+        self.timeout = 100
+        self.testProxy()
 
-    def checkProxyFormat(self, proxy):
-        try:
-            proxyMatch = re.compile(r'''http[s]?://[\d]{1,3}\.[\d]{1,3}\.[\d]{1,3}\.[\d]{1,3}:[\d]{1,5}$''')
-            re.search(proxyMatch, proxy).group()
-        except AttributeError:
-            tipUse()
-            exit()
-
-        flag = 1
-        proxy = proxy.replace('//', '')  # 去掉//
-        try:
-            protocol = proxy.split(':')[0]  # http
-            # print protocol
-            ip = proxy.split(':')[1]  # 192.168.1.183
-            port = proxy.split(':')[2]  # 8080
-
-        except IndexError:
-            print(u'下标出界')
-            tipUse()
-            exit()
-        flag = flag and len(proxy.split(':')) == 3 and len(ip.split('.')) == 4
-
-        flag = ip.split('.')[0] in map(str, range(1, 256)) and flag
-        flag = ip.split('.')[1] in map(str, range(1, 256)) and flag
-        flag = ip.split('.')[2] in map(str, range(1, 256)) and flag
-        flag = ip.split('.')[3] in map(str, range(1, 256)) and flag
-
-        flag = protocol in [u'http', u'https'] and flag
-
-        flag = port in map(str, xrange(1, 65536)) and flag
-
-        '''检查是在proxy的格式'''
-
-        if flag:
-            print(u'输入的http代理服务器符合标准')
-        else:
-            tipUse()
-            exit()
-
-    def useProxy(self, proxy):
+    def checkProxy(self):
         """
-        利用代理访问百度，并查找关键词
-        :param proxy:
+        检查代理服务地址的格式是否正确
         :return:
         """
-        protocol = proxy.split('//')[0].replace(':', '')
-        ip = proxy.split('//')[1]
-
-        opener = urllib2.build_opener(urllib2.ProxyHandler(proxies={protocol: ip}))
-        urllib2.install_opener(opener)
-
+        # 是否是正确的代理地址
+        proxyMatch = re.compile(r'http[s]?://[\d]{1,3}\.[\d]{1,3}\.[\d]{1,3}\.[\d]{1,3}:[\d]{1,5}$')
         try:
-            resp = urllib2.urlopen(self.url, timeout=self.timeout)
-        except Exception as e:
-            print(u'链接错误 {}，退出程序'.format(e))
+            re.search(proxyMatch, self.proxy).group()
+        except AttributeError as e:
+            # print(e)
+            tipUse()
+            exit()
+        part = urlparse.urlparse(self.proxy)
+        schema = part.scheme
+        self.host = part.hostname
+        self.port = part.port
+        if schema not in (u'http', u'https'):
+            print(u'协议错误')
+            tipUse()
+            exit()
+        else:
+            self.flag = True
+        # 开始判断ip地址是否正确
+        ipList = self.host.split('.')
+        # print ipList
+        for num in ipList:
+            self.flag = int(num)<256 and int(num)>=0 and self.flag
+
+        # 最后检查
+        if not self.flag:
+            print(u'ip地址范围问题')
+            tipUse()
+            return False
+        else:
+            return True
+
+    def testProxy(self):
+        """
+        测试代理
+        :return:
+        """
+        if not self.checkProxy():
             exit()
 
+        opener = urllib2.build_opener(urllib2.ProxyHandler({self.host:self.port}))
+        urllib2.install_opener(opener)
+        try:
+            resp = urllib2.urlopen(self.url,timeout=self.timeout)
+        except urllib2.URLError:
+            print(u'URL连接失败，请重试')
+            tipUse()
+            exit()
         else:
-            str = resp.read()
-            if re.search(self.flagWord, str):
-                print(u'已经取得特征詞，该代理可用')
+            if self.flagWord in resp.read():
+                print(u'连接成功，获取到特征词，该代理不可用')
             else:
                 print(u'该代理不可用')
 
-
 if __name__ == '__main__':
-    testArgument()
+    if testArgs():
+        TestProxy(sys.argv[1])
+    else:
+        exit()
